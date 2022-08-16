8dbcd55852c8e02bac6abe6efb02f8424b745e9e
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-08-16 12:45:54 +0300

    inary

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from binary_tree import BinaryTree
 tree = BinaryTree(Node(9))
 tree.add(Node(5))
 tree.add(Node(11))
-tree.add(Node(5))
+
 
 print(tree.head)
 print(tree.head.left)
 print(tree.head.right)
 
+tree.inorder()
+
+print(tree.find(5))
--- binary_tree.py
+++ binary_tree.py
@@ -24,4 +24,46 @@ class BinaryTree:
                     current_node.right = new_node
                     break
 
+    def find(self, value: int):
+        current_node = self.head
+        while current_node:
+            if value == current_node.value:
+                return current_node
+            elif value > current_node.value:
+                current_node = current_node.right
+            else:
+                current_node = current_node.left
+
+        raise LookupError(f"A node with value {value} was not found")
+
+
+    def inorder(self):
+        self._inorder_recursive(self.head)
+
+    def _inorder_recursive(self, current_node):
+        if not current_node:
+            return
+        self._inorder_recursive(current_node.left)
+        print(current_node)
+        self._inorder_recursive(current_node.right)
+
+    def find_parent(self, value: int) -> Node:
+        if self.head and self.head.value == value:
+            return self.head
+        current_node = self.head
+        while current_node:
+            if (current_node.left and current_node.left.value == value) or\
+                (current_node.right and current_node.right.value == value):
+                return current_node
+            elif value > current_node.value:
+                current_node = current_node.right
+            elif value < current_node.value:
+                current_node = current_node.left
+
+    def find_rightmost(self, node: Node) -> Node:
+        current_node = node
+        while current_node.right:
+            current_node = current_node.right
+        return current_node
+
 
